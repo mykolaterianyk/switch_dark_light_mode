165625c28e584376d5095712d2664351e1f4d43e
Mykola <mykola.terianyk@drzewniak.slupsk.pl>
2023-02-13 09:01:18 +0100

    Dark/Light mode switch

--- lib/main.dart
+++ lib/main.dart
@@ -5,63 +5,36 @@ void main() {
   runApp(const MyApp());
 }
 
-ThemeData _lightTheme = ThemeData(
-    brightness: Brightness.light
-);
-ThemeData _darkTheme = ThemeData(
-    brightness: Brightness.dark
-);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-
-
-
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  bool _switch = false;
+  final ThemeData _dark = ThemeData(brightness: Brightness.dark);
+  final ThemeData _light = ThemeData(brightness: Brightness.light);
   @override
   Widget build(BuildContext context) {
-    bool value = false;
     return MaterialApp(
-
-      theme: value ? _darkTheme: _lightTheme,
-      title: 'Switch_dart_light_mode',
-      home: const MyHomePage(),
-      Scalff
+      debugShowCheckedModeBanner: false,
+      theme: _switch? _dark : _light,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CupertinoSwitch(
+              activeColor: const Color.fromARGB(255, 45,155,240),
+              value: _switch,
+              onChanged:(_newvalue)=>setState(()=>_switch = _newvalue),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-
-  @override
-  Widget build(BuildContext context) =>Scaffold(
-
-  );
-
-  Widget buildSwitch() =>CupertinoSwitch(
-      activeColor: const Color.fromARGB(255, 45,155,240),
-      value: value,
-      onChanged:(value)=>setState(()=>this.value = value),
-    );
-
 }
